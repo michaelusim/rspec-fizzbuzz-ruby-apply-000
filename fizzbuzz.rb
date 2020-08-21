6d18101a71ad191744acfbe7242181079321be34
michael onyeije <monyeije16@saintpeters.edu>
2020-08-21 20:28:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
+
+string = ""
 def fizzbuzz
-  if val % 
-  
+  if val % 3 ==0 
+    string + "fizz"
+  elsif val % 5 == 0 
+   string + ""
+    
+    
 end 
